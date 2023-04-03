24e4e539c04dca09289c184e8073f485c25a720a
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-03 21:48:50 +0300

    2. CSharp Basic Features Courses

---
+++ 2. CSharp Basic Features/2. CSharp Basic Features Courses/05CharTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05CharTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The char type can contain one Unicode character 
+            char VarChar1 = 'a'; Console.WriteLine(VarChar1); // a
+            char VarChar2 = 'ю'; Console.WriteLine(VarChar2); // ю
+            char VarChar3 = '!'; Console.WriteLine(VarChar3); // !
+            char VarChar4 = '&'; Console.WriteLine(VarChar4); // &
+            char VarChar5 = '9'; Console.WriteLine(VarChar5); // 9
+            char VarChar6 = '§'; Console.WriteLine(VarChar6); // 󠅒󠅒
+        }
+    }
+}
