cee5ee42917f56850f5a3968b5359ec09d7701ae
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-09 14:40:14 +0300

    3. Control structures branching and loops

---
+++ 3. Control structures branching and loops/3. Branching and loops Courses/18GuessingGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _18GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 6.
+            // A customer asks you to write a guessing game in which the player must guess a number from the range 1 to 100 in a minimum number of steps.
+            // At the same time, after the next attempt, the player must see the message
+            // "The number entered is greater than the number guessed. Try again."
+            // "The entered number is less than the guessed number. Try again."
+            // or "Congratulations, the number is guessed! Number of attempts: {number}.
+
+            //  It is impossible to solve this problem without cycles (loops)
+        }
+    }
+}
