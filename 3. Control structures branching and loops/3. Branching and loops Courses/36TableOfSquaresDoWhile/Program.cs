bff56785ce2031ab075edc6b961b88fc7030a383
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-09 14:50:12 +0300

    3. Control structures branching and loops

---
+++ 3. Control structures branching and loops/3. Branching and loops Courses/36TableOfSquaresDoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _36TableOfSquaresDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 2. A customer asks you to write a program in which the user enters an integer number start.
+            // The program should output a table of squares of numbers from start to start+10
+
+            Console.Write("Enter start: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            int i = start;
+            do
+            {
+                Console.WriteLine($"{i,4}^2 = {i * i}");
+                i++;
+            } while (i <= start + 10);
+        }
+    }
+}
---
+++ 3. Control structures branching and loops/3. Branching and loops Courses/38ArithmeticAverageDoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _38ArithmeticAverageDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 4. A customer asks you to write a program to calculate the average score received by the students on the exam
+            // (!) A user inputs 10 numbers from the keyboard. Find the arithmetic mean of these numbers
+
+            double value;
+            double average = 0;
+            int i = 1;
+
+            do
+            {
+                Console.Write($"Enter {i}-e note: ");
+                value = Convert.ToDouble(Console.ReadLine());
+                average += value;
+                i++;
+            } while (i <= 10);
+
+            Console.WriteLine($"Average = {average / 10}");
+        }
+    }
+}
