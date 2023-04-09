850534f361e5531d6dae50486352d4167944a97c
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-09 14:41:36 +0300

    3. Control structures branching and loops

---
+++ 3. Control structures branching and loops/3. Branching and loops Courses/21ReiterationLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _21ReiterationLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1. Generate 10 random numbers from -8 to 9
+
+            //Random rand = new Random();
+            //Console.WriteLine($"1-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"2-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"3-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"4-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"5-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"6-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"7-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"8-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"9-e random number {rand.Next(-8, 10)}");
+            //Console.WriteLine($"10-e random number {rand.Next(-8, 10)}");
+
+
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i + 1}-e random number {r.Next(-8, 10)}  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
