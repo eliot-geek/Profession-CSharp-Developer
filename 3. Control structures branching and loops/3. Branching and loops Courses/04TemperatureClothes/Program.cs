8f2ffb61c91adc80a63b227f23e0ddce4c4983ab
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-09 14:25:07 +0300

    3. Control structures branching and loops

---
+++ 3. Control structures branching and loops/3. Branching and loops Courses/04TemperatureClothes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04TemperatureClothes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The customer asks you to write a program that determines the current temperature and recommends clothing for the walk
+
+            // The Random class is a built-in class in C# that allows you to generate random numbers.
+            Random rand = new Random();
+
+            int temperature = rand.Next(30);
+            Console.WriteLine($"Current temperature = {temperature} c");
+
+            if (temperature < 18)
+            {
+                Console.WriteLine("It's chilly outside, so I recommend a warmer jacket.");
+            }
+            else
+            {
+                Console.WriteLine("It's warm outside, you don't need a jacket.");
+            }
+        }
+    }
+}
