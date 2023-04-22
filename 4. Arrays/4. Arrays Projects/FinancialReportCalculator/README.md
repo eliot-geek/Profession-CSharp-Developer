b08c2d6be1ffbad6b6b342cf39825376b1c4c907
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-22 16:29:36 +0300

    4. Arrays

---
+++ 4. Arrays/4. Arrays Projects/FinancialReportCalculator/README.md
@@ -0,0 +1,43 @@
+Task 1.
+
+The customer asks you to write an application for accounting finances and demonstrate its operation.
+
+The essence of the task is as follows:
+
+The management of the company keeps track of income and expenses over 12 months.
+
+Two arrays are obtained for the year -expenses and income.
+
+Determine profits by month
+
+Number of months with positive profit.
+
+Add the ability to output the three worst indicators by month, with the worst profit,
+
+if there are several months, in some of which the worst profit coincided - display all of them.
+
+Organize a user - friendly interface like this 
+
+
+Example
+
+
+Month            Income, $         Expenses, $             Profit, $ 
+    1              100,000              80,000                20,000
+    2              120,000              90,000                30,000
+    3               80,000              70,000                10,000
+    4               70,000              70,000                     0
+    5              100,000              80,000                20,000
+    6              200,000             120,000                80,000
+    7              130,000             140,000               -10,000
+    8              150,000              65,000                85,000
+    9              190,000              90,000               100,000
+   10              110,000              70,000                40,000
+   11              150,000             120,000                30,000
+   12              100,000              80,000                20,000
+   
+   
+
+Worst profit in months: 7, 4, 1, 5, 12
+
+Months with positive profit: 10
---
+++ 4. Arrays/4. Arrays Projects/PascalTriangleEasyMode/Program.cs
@@ -0,0 +1,109 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+
+namespace PascalTriangleEasyMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 2
+
+            // The client needs an application that builds the first N rows of Pascal's triangle. N < 25.
+            // When N = 9, the triangle looks like this:
+            // 1
+            // 1       1
+            // 1       2       1
+            // 1       3       3       1
+            // 1       4       6       4       1
+            // 1       5      10      10       5       1
+            // 1       6      15      20      15       6       1
+            // 1       7      21      35      35       21      7       1
+
+            // Pascal's triangle is a triangular array of numbers in which the first and last numbers of each row are 1,
+            // and each of the remaining numbers is the sum of the two numbers directly above it. 
+            
+            // Reference - https://en.wikipedia.org/wiki/Pascal%27s_triangle
+            
+            #endregion
+
+
+            #region Solution 1
+
+            // Print the title of the program to the console
+            Console.WriteLine("Pascal triangle - Easy Mode");
+            // Create a 2D array with size of 25 x 25 - This size is chosen as a maximum size to accommodate the largest possible value of N.
+            int[,] array2D = new int[25, 25];
+            // Ask the user to input an integer value for N
+            Console.Write("Enter n (N < 25): ");
+            // Read the user input as an integer and store it in the variable n
+            int n = int.Parse(Console.ReadLine());
+
+            // Start a loop to generate the rows of the Pascal's triangle
+            for (int i = 0; i < n; i++)
+            {
+                // Check if the user input value exceeds the array size limit, if yes, keep asking for input until it is within the limit
+                while (n > 25)
+                {
+                    Console.Write("You entered more than your memory allotment. Again : ");
+                    n = int.Parse(Console.ReadLine());
+                }
+                // Generate the numbers for each row of the triangle and store them in the array
+                for (int j = 0; j <= i; j++)
+                {
+                    // the first and last elements in each row are always 1
+                    if (j == 0 || i == j)
+                    {
+                        array2D[i, j] = 1;
+                    }
+                    else
+                    {
+                        array2D[i, j] = array2D[i - 1, j] + array2D[i - 1, j - 1];
+                    }
+                    // Print each number to the console
+                    Console.Write(array2D[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+
+            #region Solution 2
+            
+            Console.WriteLine("\nSolution 2");
+            Console.Write("Enter the number of rows to generate: ");
+            int numb = int.Parse(Console.ReadLine());
+
+            // create a 2D jagged array to hold the triangle
+            int[][] triangle = new int[numb][]; 
+            for (int i = 0; i < numb; i++)
+            {
+                // create an array for each row with length i+1
+                triangle[i] = new int[i + 1];   
+                for (int j = 0; j <= i; j++)
+                {
+                    // the first and last elements in each row are always 1
+                    if (j == 0 || j == i)       
+                    {
+                        triangle[i][j] = 1;
+                    }
+                    else   // the other elements are the sum of the two elements above them
+                    {
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                    }
+                }
+            }
+            // print out the triangle
+            for (int i = 0; i < numb; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(triangle[i][j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+        }
+    }
+}
