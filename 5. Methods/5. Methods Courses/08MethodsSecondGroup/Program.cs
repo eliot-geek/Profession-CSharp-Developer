cbed14d58d359d1cdc05abd1fb1b14d5c80f64c2
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-29 20:31:04 +0300

    5. Methods

---
+++ 5. Methods/5. Methods Courses/08MethodsSecondGroup/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _08MethodsSecondGroup
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// A method to delay console closure by Interval seconds 
+        /// </summary>
+        /// <param name="Interval">Number of seconds until the console closes</param>
+        static void Pause(int Interval)
+        {
+            Thread.Sleep(Interval * 1000);
+        }
+
+        /// <summary>
+        /// Output a personalized greeting to the console
+        /// </summary>
+        /// <param name="Name">The name to be greeted</param>
+        static void Hi(string Name)
+        {
+            Random r = new Random();
+            string text = string.Empty;
+
+            switch (r.Next(5))
+            {
+                case 0: 
+                    text = $"Hello, {Name}!"; 
+                    break;
+                case 1: 
+                    text = $"Good morning, {Name}!"; 
+                    break;
+                case 2: 
+                    text = $"Greetings to you, {Name}!"; 
+                    break;
+                case 3: 
+                    text = $"Glad to see you, {Name}!"; 
+                    break;
+                default: 
+                    text = $"I've been waiting for you, {Name}!"; 
+                    break;
+            }
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Arranged clutter on the screen the desired number of times
+        /// </summary>
+        /// <param name="Text1">First text to be displayed on the screen</param>
+        /// <param name="Text2">Second text to be displayed on the screen</param>
+        /// <param name="Count">How many times to output one of the randomly selected texts</param>
+        static void ApplauseAndOvations(string Text1, string Text2, int Count)
+        {
+            Random r = new Random();
+            int max = new int[] { Text1.Length, Text1.Length }.Max();
+
+            for (int i = 0; i < Count; i++)
+            {
+                int posX = r.Next(Console.WindowWidth - max);       // Get console width
+                int posY = r.Next(Console.WindowHeight);            // Get console height
+                Console.SetCursorPosition(posX, posY + 1);          // Set the cursor to a random position on the screen
+                Console.Write(r.Next(2) == 0 ? Text1 : Text2);      // Output one of the joyful messages
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region 02. Second Group
+            ////
+            ////    │   Something   │
+            ////    │     takes     │
+            ////    │   arguments   │
+            //// ┌──┘               └────────────────────────┐
+            //// │                                           │
+            //// │              Doing something              │
+            //// │                                           │
+            //// └───────────────────────────────────────────┘
+            // 
+            //   static void MethodName(FormalParameters)
+            //   {
+            //
+            //   }
+            #endregion
+
+            Hi("Elon");                                     // Calling the method Hi
+            ApplauseAndOvations("Waouhh!", "Dammmm!", 70);  // Calling the method ApplauseAndOvations
+            Pause(10);                                      // Calling the method Pause
+        }
+    }
+}
---
+++ 5. Methods/5. Methods Courses/09MethodsThirdGroup/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace _09MethodsThirdGroup
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Method for generating a string from a set of random characters
+        /// </summary>
+        /// <returns>Random string</returns>
+        static string GetRandomString()
+        {
+            // Guid.NewGuid() method creates a unique identifier (GUID) that can be used as a random string
+            // ToString() method is called to convert the GUID to a string
+            var result = Guid.NewGuid().ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// A method that returns a random greeting
+        /// </summary>
+        /// <param name="Name">Random greeting</param>
+        static string GetRandomHi()
+        {
+            Random r = new Random();
+            string text = string.Empty;
+
+            switch (r.Next(5))
+            {
+                case 0:
+                    text = $"Hello!";
+                    break;
+                case 1:
+                    text = $"Good morning!";
+                    break;
+                case 2:
+                    text = $"Greetings to you!";
+                    break;
+                case 3:
+                    text = $"Glad to see you!";
+                    break;
+                default:
+                    text = $"I've been waiting for you!";
+                    break;
+            }
+            return text;
+        }
+
+        #region VS2022 - THIS METHOD DOESN'T WORK ON VISUAL STUDIO 2022
+        // A method that returns the full path to the current application BUT IT DOESN'T WORK ON VISUAL STUDIO 2022
+        
+        //static string GetPath()
+        //{
+        //    return Application.ExecutablePath;
+        //}
+
+        //res = GetPath(); 
+        //Console.WriteLine(res);
+        #endregion
+
+        /// <summary>
+        ///  method that returns the full path to the current application
+        /// </summary>
+        /// <returns>Full path to the current application</returns>
+        public static string GetCurrentApplicationPath()
+        {
+            return Assembly.GetExecutingAssembly().Location;
+        }
+
+        static void Main(string[] args)
+        {
+            #region 03. Third Group
+
+            ////
+            //// 
+            ////
+            //// ┌───────────────────────────────────────────┐
+            //// │                                           └──────
+            //// │              Doing something                     >>> gives out the result of his work
+            //// │                                           ┌──────
+            //// └───────────────────────────────────────────┘
+            ////
+            // 
+            //   static TypeData MethodName()
+            //   {
+            //      
+            //   }
+
+            #endregion
+
+            string res = GetRandomString(); 
+            Console.WriteLine(res);             // 1c43db96-0b95-48f4-a6c3-217068da97da
+            res = GetRandomHi(); 
+            Console.WriteLine(res);             // Greetings to you!
+            res = GetCurrentApplicationPath();
+            Console.WriteLine(res);             // C:\Users\leosa\Downloads\PushHub\5. Methods\5. Methods Courses\MethodsCourses\09MethodsThirdGroup\bin\Debug\net6.0\09MethodsThirdGroup.dll
+            Console.ReadKey();
+        }
+    }
+}
