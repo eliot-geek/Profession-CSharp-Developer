5bf196a664e9b8fc53fc6d3fc8e99fe10fa11583
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-29 20:32:42 +0300

    5. Methods

---
+++ 5. Methods/5. Methods Courses/10MethodsFourthGroup/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _10MethodsFourthGroup
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Method for generating a string from a set of random characters
+        /// </summary>
+        /// <param name="Count">Number of characters</param>
+        /// <returns>Random string</returns>
+        static string GetRandomString(int Count)
+        {
+            return Guid.NewGuid().ToString().Substring(0, Count);
+        }
+
+        /// <summary>
+        /// Method that returns the full path to the current application
+        /// </summary>
+        /// <returns>Full path to the current application</returns>
+        static string GetCurrentApplicationPath(bool FileName)
+        {
+            // If "FileName" is true, the method returns the full path of the currently executing assembly, including the file name itself
+            // If "FileName" is false, the method returns the path of the directory that contains the calling assembly 
+            return FileName ? Assembly.GetExecutingAssembly().Location : Assembly.GetCallingAssembly().Location;
+        }
+
+        /// <summary>
+        /// A method that returns the maximum of two numbers
+        /// </summary>
+        /// <param name="Value1">First number</param>
+        /// <param name="Value2">Second number</param>
+        /// <returns>Maximum number</returns>
+        static int Max(int Value1, int Value2)
+        {
+            return Value1 > Value2 ? Value1 : Value2;
+        }
+
+        /// <summary>
+        /// This method takes an array of integers as input and returns a new array containing the negation of each element in the input array.
+        /// </summary>
+        /// <param name="Args">Input array</param>
+        /// <returns>Converted array</returns>
+        static int[] Inverse(int[] Args)
+        {
+            // Create a new array of the same length as the input array.
+            int[] t = new int[Args.Length];
+            // Loop over each element in the input array and negate it.
+            for (int i = 0; i < Args.Length; i++)
+            {
+                t[i] = -Args[i];
+            }
+            // Return the new array containing the negated elements.
+            return t;
+        }
+
+        static void Main(string[] args)
+        {
+            #region 04. Fourth Group
+            //// 
+            ////
+            ////    │  Something    │
+            ////    │     takes     │
+            ////    │  arguments    │
+            //// ┌──┘               └────────────────────────┐
+            //// │                                           └──────
+            //// │              Doing something                     >>> gives out the result of his work
+            //// │                                           ┌──────
+            //// └───────────────────────────────────────────┘
+            //
+            // 
+            //   static DataType MethodName(Formal Parameters)
+            //   {
+            //      
+            //   }
+
+            #endregion
+
+            Console.WriteLine(GetRandomString(10));    // Calling the method GetRandomString
+            Console.WriteLine(GetRandomString(15));   // Calling the method GetRandomString
+            Console.WriteLine(GetRandomString(25));   // Calling the method GetRandomString
+            Console.WriteLine();
+
+            Console.WriteLine(GetCurrentApplicationPath(true));         // Calling the method GetCurrentApplicationPath
+            Console.WriteLine(GetCurrentApplicationPath(false));        // Calling the method GetCurrentApplicationPath
+            Console.WriteLine();
+
+
+            int[] col = { 1, 3, 5, 7, 9 };
+            foreach (var e in col) 
+                Console.Write($"+{e} ");
+            Console.WriteLine();
+
+            int[] inverseCol = Inverse(col);                            // Calling the method Inverse
+            foreach (var e in inverseCol) 
+                Console.Write($"{e} ");
+            Console.WriteLine();
+
+            #region NOT WORKING
+            //static string GetPath(bool FileName)
+            //{
+            //    return FileName ? Application.ExecutablePath : Application.StartupPath;
+            //}
+
+            //Console.WriteLine(GetPath(true));         
+            //Console.WriteLine(GetPath(false));       
+            #endregion
+        }
+    }
+}
---
+++ 5. Methods/5. Methods Courses/12OverloadingMethods/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace _12OverloadingMethods
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The method that fills the array Col
+        /// </summary>
+        /// <param name="Col">An array of integers to fill</param>
+        static void Fill(int[] Col)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Col.Length; i++)
+            {
+                Col[i] = r.Next(100);
+            }
+        }
+
+        /// <summary>
+        /// The method that fills the first Count elements of the array Col
+        /// </summary>
+        /// <param name="Col">The array of integers to be filled</param>
+        /// <param name="Count">Number of elements to change</param>
+        static void Fill(int[] Col, int Count)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Count; i++)
+            {
+                Col[i] = r.Next(100);
+            }
+        }
+
+        /// <summary>
+        /// A method that fills the first Count elements of the Col array with numbers from the BottomRange UpperRange.
+        /// BottomRange must be strictly smaller than the UpperRange
+        /// </summary>
+        /// <param name="Col">An array of integers to fill</param>
+        /// <param name="Count">Number of elements to change</param>
+        /// <param name="BottomRange">Lower boundary of the range</param>
+        /// <param name="UpperRange">Higher range limit</param>
+        static void Fill(int[] Col, int Count, int BottomRange, int UpperRange)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Count; i++)
+            {
+                Col[i] = r.Next(BottomRange, UpperRange);
+            }
+        }
+
+        /// <summary>
+        /// A method that fills elements of the Col array with numbers from the BottomRange UpperRange.
+        /// BottomRange must be strictly smaller than the UpperRange
+        /// </summary>
+        /// <param name="Col">An array of integers to fill</param>
+        /// <param name="BottomRange">BottomRange</param>
+        /// <param name="UpperRange">Upper range limit</param>
+        static void Fill(int[] Col, int BottomRange, int UpperRange)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Col.Length; i++)
+            {
+                Col[i] = r.Next(BottomRange, UpperRange);
+            }
+        }
+
+        /// <summary>
+        /// A method that fills elements of the Col array with numbers from the BottomRange UpperRange.
+        /// BottomRange must be strictly smaller than the UpperRange
+        /// </summary>
+        /// <param name="Col">An array of integers to fill</param>
+        /// <param name="BottomRange">Lower boundary of the range</param>
+        /// <param name="UpperRange">Upper limit of the range</param>
+        static void Fill(double[] Col, int BottomRange, int UpperRange)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Col.Length; i++)
+            {
+                Col[i] = r.Next(BottomRange, UpperRange);
+            }
+        }
+
+        /// <summary>
+        /// The method that outputs the array Col to the console
+        /// </summary>
+        /// <param name="Col">An array of integers to output to the console</param>
+        static void Print(int[] Col)
+        {
+            Random r = new Random();
+            for (int i = 0; i < Col.Length; i++)
+            {
+                Console.Write($"{Col[i],3}");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            #region Method overloading
+            // Method overloading is a set of methods with the same name, but with a different number or type of formal parameters
+            #endregion
+
+            int[] arr = new int[20];
+
+            Fill(arr, 10);              // static void Fill(int[] Col, int Count)
+            Print(arr);
+            Console.ReadKey();
+
+            Fill(arr);                  // static void Fill(int[] Col)
+            Print(arr);
+            Console.ReadKey();
+
+            Fill(arr, -10, 0);          // static void Fill(int[] Col, int BottomRange, int UpperRange)
+            Print(arr);
+            Console.ReadKey();
+
+            Fill(arr, 10, 1, 30);       // static void Fill(int[] Col, int Count, int BottomRange, int UpperRange)
+            Print(arr);
+            Console.ReadKey();
+
+        }
+    }
+}
