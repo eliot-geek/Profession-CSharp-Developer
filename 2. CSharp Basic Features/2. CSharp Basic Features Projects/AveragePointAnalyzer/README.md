d26571304c72cba91446ebf19df56acae27fc106
eliot <leosarah109@gmail.com>
2023-10-19 11:51:20 +0300

    2. Basic Features

---
+++ 2. CSharp Basic Features/2. CSharp Basic Features Projects/AveragePointAnalyzer/README.md
@@ -0,0 +1,14 @@
+# Task 2: Implementation of Score Calculation for All Subjects
+
+## What Needs to be Done
+Create two variables to calculate the total score for all subjects and calculate the arithmetic average. To do this:
+
+1. Create a variable for the total score for all subjects.
+2. Calculate the sum of scores.
+3. Create a variable for the average score.
+4. Calculate the arithmetic average of the scores.
+5. Display the information on the screen.
+
+## Tips and Recommendations
+The arithmetic average is calculated as the sum of all elements divided by the number of elements.
+Display the information after pressing any key (use `Console.ReadKey` after displaying the information from the first task).
---
+++ 2. CSharp Basic Features/2. CSharp Basic Features Projects/VariablesDeclarationOutput/VariablesDeclarationOutput/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace VariablesDeclarationOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string fullName = "Anders Hejlsberg";
+            int age = 62;
+            string email = "andershejlsberg@gmail.com";
+            double programmingScores = 95.5;
+            double mathScores = 87.2;
+            double physicsScores = 91.7;
+
+            // Display information on the screen
+            Console.WriteLine("Full Name: {0}", fullName);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Email: {0}", email);
+            Console.WriteLine("Programming Scores: {0}", programmingScores);
+            Console.WriteLine("Math Scores: {0}", mathScores);
+            Console.WriteLine("Physics Scores: {0}", physicsScores);
+        }
+    }
+}
