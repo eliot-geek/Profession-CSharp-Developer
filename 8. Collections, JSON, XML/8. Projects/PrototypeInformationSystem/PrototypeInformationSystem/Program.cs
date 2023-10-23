005c407d625d3315d94e258e6866f068e3a01ed7
eliot <leosarah109@gmail.com>
2023-10-24 01:19:32 +0300

    8. Projects

---
+++ 8. Collections, JSON, XML/8. Projects/PrototypeInformationSystem/PrototypeInformationSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrototypeInformationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
