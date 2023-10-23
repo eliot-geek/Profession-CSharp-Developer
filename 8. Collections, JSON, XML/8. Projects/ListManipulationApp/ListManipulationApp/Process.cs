b05627e49945cb1d7e24483c1d0aa4284167d1ad
eliot <leosarah109@gmail.com>
2023-10-23 12:20:02 +0300

    8. Projects

---
+++ 8. Collections, JSON, XML/8. Projects/ListManipulationApp/ListManipulationApp/Process.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManipulationApp
+{
+    /// <summary>
+    /// Process the list
+    /// </summary>
+    internal class Process
+    {
+        /// <summary>
+        /// Create a list of integers and fill with random numbers 
+        /// </summary>
+        /// <returns></returns>
+        public static List<int> CreatedList()
+        {
+            List<int> collection = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                collection.Add(random.Next(0, 100));
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// Display the collection
+        /// </summary>
+        /// <param name="collection"></param>
+        public static void PrintList(List<int> collection)
+        {
+            Console.WriteLine($"The list contains {collection.Count} integers. ");
+            foreach (int item in collection)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        /// <summary>
+        /// Remove from the list numbers that are greater than 25, but less than 50.
+        /// </summary>
+        /// <param name="collection"></param>
+        public static void RemoveCondition(List<int> collection)
+        {
+            Console.WriteLine("\n\nRemove numbers greater than 25, but less than 50");
+            collection.RemoveAll(e => e > 25 && e < 50);
+        }
+    }
+}
---
+++ 8. Collections, JSON, XML/8. Projects/ListManipulationApp/ListManipulationApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ListManipulationApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> collection = Process.CreatedList();
+        Process.PrintList(collection);
+        Process.RemoveCondition(collection);
+        Process.PrintList(collection);
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
