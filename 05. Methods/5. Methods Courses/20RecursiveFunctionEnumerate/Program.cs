3f94ae7639f3eed2b39c1df1f3feced8a29fcb85
Eliot <45435800+eliot-geek@users.noreply.github.com>
2024-01-04 18:16:16 +0300

    05. Methods

--- 05. Methods/5. Methods Courses/20RecursiveFunctionEnumerate/Program.cs
+++ 05. Methods/5. Methods Courses/20RecursiveFunctionEnumerate/Program.cs
@@ -10,36 +10,32 @@
         /// <param name="N"></param>
         static void Find(char[] Set, char[] Word, int N)
         {
-            // If N equals the length of the Word array
             if (N == Word.Length)
             {
-                // Output each element of the Word array
                 foreach (var e in Word) 
                 { 
                     Console.Write(e); 
                 }
-                Console.WriteLine();    // Output a new line
-                return;                 // Exit the function
+                
+                Console.WriteLine();   
+                return;                
             }
 
-            // Create a new character array tWord and assign it to Word
             char[] tWord = Word;
 
-            // Loop through the elements in the Set array
             for (int i = 0; i < Set.Length; i++)   
             {
-                tWord[N] = Set[i];              // Assign the current element of Set to the Nth index of tWord
-                Find(Set, tWord, N + 1);        // Call the Find function recursively with updated arguments
+                tWord[N] = Set[i];              
+                Find(Set, tWord, N + 1);       
             }
         }
 
 
         static void Main(string[] args)
         {
-            char[] alphabet = { 'q', 'w', 'e' };                // Define an array of characters named alphabet
-            char[] currentWord = new char[3];                   // Define an array of characters named currentWord with length 4
-
-            Find(alphabet, currentWord, 0);                     // Call the Find function with the defined arguments
+            char[] alphabet = { 'q', 'w', 'e' };               
+            char[] currentWord = new char[3];       
+            Find(alphabet, currentWord, 0); 
         }
     }
 }
