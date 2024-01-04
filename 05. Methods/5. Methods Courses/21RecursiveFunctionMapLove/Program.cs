6827d1ce3bdf2a5059e651878723fda871647f0c
Eliot <45435800+eliot-geek@users.noreply.github.com>
2024-01-04 18:21:10 +0300

    05. Methods

--- 05. Methods/5. Methods Courses/21RecursiveFunctionMapLove/Program.cs
+++ 05. Methods/5. Methods Courses/21RecursiveFunctionMapLove/Program.cs
@@ -8,20 +8,16 @@
         /// <param name="Map"></param>
         static void Print(char[,] Map)
         {
-            // Loop through the first dimension of Map
             for (int i = 0; i < Map.GetLength(0); i++)
             {
-                // Loop through the second dimension of Map
                 for (int j = 0; j < Map.GetLength(1); j++)
                 {
-                    // Output a space character if the current element of Map is a dot, otherwise output the current element
                     Console.Write(Map[i, j] == '.' ? ' ' : Map[i, j]);  
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
         }
-
 
         /// <summary>
         /// Define a function named ToColor that takes in a 2D character array named Map and two integers named PosX and PosY as arguments
@@ -34,11 +30,11 @@
             // If the current element of Map is a dot
             if (Map[PosX, PosY] == '.')
             {
-                Map[PosX, PosY] = '+';        // Change the current element to a plus sign to "color" it = "color" the current cell
-                ToColor(Map, PosX - 1, PosY); // Recursively call ToColor with the cell above the current one as arguments
-                ToColor(Map, PosX, PosY + 1); // Recursively call ToColor with the cell to the right of the current one as arguments
-                ToColor(Map, PosX + 1, PosY); // Recursively call ToColor with the cell below the current one as arguments
-                ToColor(Map, PosX, PosY - 1); // Recursively call ToColor with the cell to the left of the current one as arguments
+                Map[PosX, PosY] = '+';                // Change the current element to a plus sign to "color" it = "color" the current cell
+                ToColor(Map, PosX - 1, PosY);         // Recursively call ToColor with the cell above the current one as arguments
+                ToColor(Map, PosX, PosY + 1);         // Recursively call ToColor with the cell to the right of the current one as arguments
+                ToColor(Map, PosX + 1, PosY);         // Recursively call ToColor with the cell below the current one as arguments
+                ToColor(Map, PosX, PosY - 1);         // Recursively call ToColor with the cell to the left of the current one as arguments
             }
         }
 
@@ -56,7 +52,7 @@
                 {'.','.','.','+','.','.','.', }
             };
 
-            Print(map);           // Call the Print function with map as an argument to print the contents of map
+            Print(map);          
             Console.ReadKey();
 
             ToColor(map, 1, 1);    // Call the ToColor function with map and (1,1) as arguments to "color" the connected cells
