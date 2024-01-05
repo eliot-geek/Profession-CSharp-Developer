3d3f9b3ba61c73e992056e1cdc8a15c2f6c5f87e
eliot <leosarah109@gmail.com>
2024-01-05 13:50:05 +0300

    05. Methods

--- 05. Methods/5. Methods Projects/MethodLineDividerInWords/MethodLineDividerInWords/Program.cs
+++ 05. Methods/5. Methods Projects/MethodLineDividerInWords/MethodLineDividerInWords/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MethodLineDividerInWords
+﻿using System.Text;
+
+namespace MethodLineDividerInWords
 {
     internal class Program
     {
@@ -13,12 +15,46 @@
         }
 
         /// <summary>
+        /// Splits a given string into words manually using a loop.
+        /// </summary>
+        /// <param name="text">The input string to be split into words.</param>
+        /// <returns>An array of words.</returns>
+        static string[] SplitTextManually(string text)
+        {
+            List<string> wordsList = new List<string>();
+            StringBuilder currentWord = new StringBuilder();
+
+            foreach (char character in text)
+            {
+                if (character == ' ')
+                {
+                    if (currentWord.Length > 0)
+                    {
+                        wordsList.Add(currentWord.ToString());
+                        currentWord.Clear();
+                    }
+                }
+                else
+                {
+                    currentWord.Append(character);
+                }
+            }
+
+            if (currentWord.Length > 0)
+            {
+                wordsList.Add(currentWord.ToString());
+            }
+
+            return wordsList.ToArray();
+        }
+
+        /// <summary>
         /// Method to output the words on separate lines
         /// </summary>
         /// <param name="words"></param>
         static void OutputWords(string[] words)
         {
-            Console.WriteLine("Words in the sentence: ");
+            Console.WriteLine("\nWords in the sentence: ");
             foreach (string word in words)
             {
                 Console.WriteLine(word);
@@ -31,7 +67,10 @@
             string input = Console.ReadLine();
 
             string[] words = SplitText(input);
+            string[] wordsManually = SplitTextManually(input);
+
             OutputWords(words);
+            OutputWords(wordsManually);
             Console.ReadKey();
         }
     }
--- 05. Methods/5. Methods Projects/MethodLineDividerInWords/MethodLineDividerInWords/obj/Debug/net6.0/MethodLineDividerInWords.AssemblyInfo.cs
+++ 05. Methods/5. Methods Projects/MethodLineDividerInWords/MethodLineDividerInWords/obj/Debug/net6.0/MethodLineDividerInWords.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodLineDividerInWords")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b18eab3f5a49bec071995bb343dd923d14eded")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodLineDividerInWords")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodLineDividerInWords")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
