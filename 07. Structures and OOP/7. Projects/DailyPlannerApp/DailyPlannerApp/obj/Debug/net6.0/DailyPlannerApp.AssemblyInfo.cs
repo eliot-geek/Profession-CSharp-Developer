ea3719575180eec3ed77b431a9d69ac53de6ba32
eliot <leosarah109@gmail.com>
2024-01-09 22:34:44 +0300

    07. Structures and OOP

--- 07. Structures and OOP/7. Projects/DailyPlannerApp/DailyPlannerApp/obj/Debug/net6.0/DailyPlannerApp.AssemblyInfo.cs
+++ 07. Structures and OOP/7. Projects/DailyPlannerApp/DailyPlannerApp/obj/Debug/net6.0/DailyPlannerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyPlannerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a9a36138643382a709f0198663f603b773fe11")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyPlannerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyPlannerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
