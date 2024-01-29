2fcfd4e7839a444bd46d2f574564e4b1aec2f461
eliot <leosarah109@gmail.com>
2024-01-29 11:38:10 +0300

    10. Windows Presentation Foundation

---
+++ 10. Visual Programming (WPF) Technology/10. Courses/Less05TextManipulation/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Less05TextManipulation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtValue.Text = $"Position set to {Convert.ToInt32(slider.Value)}";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtValue.Text = (cb.SelectedValue as TextBlock).Text;
+        }
+    }
+}
