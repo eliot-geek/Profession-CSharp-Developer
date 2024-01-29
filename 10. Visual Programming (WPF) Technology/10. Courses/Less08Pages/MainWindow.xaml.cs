78dea4dda4604ffea388a6423b9c9f6800d541f4
eliot <leosarah109@gmail.com>
2024-01-29 12:51:57 +0300

    10. Windows Presentation Foundation

---
+++ 10. Visual Programming (WPF) Technology/10. Courses/Less08Pages/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Less08Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Page1 p1 = new Page1();
+        Page2 p2 = new Page2();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnPage1Click(object sender, RoutedEventArgs e)
+        {
+            //MainFrame.Content = new Page1();
+            MainFrame.Content = p1;
+        }
+
+        private void BtnPage2Click(object sender, RoutedEventArgs e)
+        {
+            //MainFrame.Content = new Page2();
+            MainFrame.Content = p2; 
+        }
+
+    }
+}
---
+++ 10. Visual Programming (WPF) Technology/10. Courses/Less09DynamicData/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Less09DynamicData
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Worker> db = new ObservableCollection<Worker>(); // It's better to use this
+        //List<Worker> db = new List<Worker>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listDbView.ItemsSource = db;
+        }
+
+        int count = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            db.Add(new Worker()
+            {
+                Name = $"Name {++count}",
+                Age = 20 + count,
+                Salary = 1000 * count
+            });
+
+            this.Title = $"db.Count = {db.Count}"; // On the window's head
+            listDbView.Items.Refresh(); // Delete all and reprint
+        }
+    }
+}
